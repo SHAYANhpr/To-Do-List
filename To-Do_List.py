5a43cf61718674e86f998014d39e6d12da8f0aaf
unknown <shayan.shhassanpour87@email>
2025-07-28 11:49:57 +0330

    Add def show_tasks

--- To-Do_List.py
+++ To-Do_List.py
@@ -1,13 +1,13 @@
 import csv
 
 class Task:
-    def __init__(self, name, description, olavit):
+    def __init__(self, name, description, Priority):
         self.name = name          
         self.description = description 
-        self.olavit = olavit 
+        self.Priority = Priority 
 
     def to_list(self):
-        return [self.name, self.description, self.olavit]
+        return [self.name, self.description, self.Priority]
     
 class ToDoList:
     def __init__(self):
@@ -21,3 +21,13 @@ class ToDoList:
             del self.tasks[index]
         else:
             print("The number entered is invalid.")
+
+    def show_tasks(self):
+        if not self.tasks:
+            print("The task list is empty.")
+        else:
+            print("\n To-do list:")
+            for i, task in enumerate(self.tasks):
+                print(f"{i}. name: {task.name} | description: {task.description} | Priority: {task.Priority}")
+
+    

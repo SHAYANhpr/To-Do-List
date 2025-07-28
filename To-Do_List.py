a88adbd44a2c74ac01e14016058e2f5a3afdd4d2
unknown <shayan.shhassanpour87@email>
2025-07-28 11:26:11 +0330

    Creating a Task class

---
+++ To-Do_List.py
@@ -0,0 +1,7 @@
+import csv
+
+class Task:
+    def __init__(self, name, description, olavit):
+        self.name = name          
+        self.description = description 
+        self.olavit = olavit 
